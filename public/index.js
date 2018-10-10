1f9dd5ac521e5168faf2646bde011299ec31852a
dzaghian <zaghian.dani@gmail.com>
2018-10-10 10:39:47 -0700

    adds javascript axios request and loop through data

--- public/index.js
+++ public/index.js
@@ -1,9 +1,17 @@
+/* global axios */
+
 var recipeTemplate = document.querySelector("#recipe-card");
 var recipeRow = document.querySelector(".row");
 
-recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRow.appendChild(recipeTemplate.content.cloneNode(true));	
+axios.get("http://localhost:3000/api/recipes").then(function(response){
+	var recipes = response.data;
+	console.log(recipes);
+
+	recipes.forEach(function(recipe){
+		var recipeClone = recipeTemplate.content.cloneNode(true);
+		recipeClone.querySelector(".card-img-top").src = recipe.image_url;
+		recipeClone.querySelector(".card-title").innerText = recipe.title;
+		recipeClone.querySelector(".card-text").innerText = recipe.ingredients;
+		recipeRow.appendChild(recipeClone);
+	});
+});
